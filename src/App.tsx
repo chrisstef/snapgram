08f8cb1bb18ae14b18f0c7b5718456c968addd2b
chrisstef <xristostefanis@gmail.com>
2024-03-27 13:41:55 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+const App = () => {
+    return (
+        <div className="text-2x text-off-white underline underline-offset-2 font-bold">
+            App
+        </div>
+    );
+};
+
+export default App;
