512e3d20e756da67429297e57618d6c6e62f856a
chrisstef <xristostefanis@gmail.com>
2024-03-27 13:56:36 +0200

    add folder strucutre and basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
     return (
-        <div className="text-2x text-off-white underline underline-offset-2 font-bold">
-            App
-        </div>
+        <main className="flex h-screen">
+            <Routes>
+                <Route element={<AuthLayout />}>
+                    {/* public routes */}
+                    <Route path="/sign-in" element={<SigninForm />} />
+                    <Route path="/sign-up" element={<SignupForm />} />
+                </Route>
+
+                <Route element={<RootLayout />}>
+                    {/* private routes */}
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
     );
 };
 
