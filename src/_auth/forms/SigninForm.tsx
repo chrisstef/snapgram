0d677e67c4bb87e98a3c5785519824674ec0650a
chrisstef <xristostefanis@gmail.com>
2024-03-29 12:47:35 +0200

    create frontend for signup & signin forms, overwrite globals.css & tailwind.config.js

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,7 +1,109 @@
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SigninValidation } from "@/lib/validation";
+import Loader from "@/components/shared/Loader";
 
 const SigninForm = () => {
-    return <div>SigninForm</div>;
+    const isLoading = false;
+    const form = useForm<z.infer<typeof SigninValidation>>({
+        resolver: zodResolver(SigninValidation),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    async function onSubmit(values: z.infer<typeof SigninValidation>) {
+        // const newUser = await createUserAccount(values);
+    }
+
+    return (
+        <Form {...form}>
+            <div className="sm:w-420 flex-center flex-col">
+                <img src="/assets/images/logo.svg" alt="logo" />
+                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+                    Log in to your account
+                </h2>
+                <p className="text-light-3 small-medium md:base-regular mt-2">
+                    Welcome back! Please enter your details.
+                </p>
+
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="flex flex-col gap-5 w-full mt-4"
+                >
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        type="email"
+                                        className="shad-input"
+                                        {...field}
+                                    />
+                                </FormControl>
+
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        type="password"
+                                        className="shad-input"
+                                        {...field}
+                                    />
+                                </FormControl>
+
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="shad-button_primary">
+                        {isLoading ? (
+                            <div className="flex-center gap-2">
+                                <Loader /> Loading...
+                            </div>
+                        ) : (
+                            "Log in"
+                        )}
+                    </Button>
+
+                    <p className="text-small-regular text-light-2 text-center mt-2">
+                        Don&apos;t have an account?
+                        <Link
+                            to="/sign-up"
+                            className="text-primary-500 text-small-semibold ml-1"
+                        >
+                            Sign up
+                        </Link>
+                    </p>
+                </form>
+            </div>
+        </Form>
+    );
 };
 
 export default SigninForm;
